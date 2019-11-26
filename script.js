68b924ff12d8c9ee2d5efe7f9294a5c67eaef388
AgataOstrowska <56037254+AgataOstrowska@users.noreply.github.com>
2019-11-26 17:04:38 +0100

    circle & cross
    ???

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
-function userClicked1() {
-    document.getElementById("1").innerHTML = "X"
-}
-                        ///OTHER CLICKED (2-9)///
-function userClicked2() {
-    document.getElementById("2").innerHTML = "X"
+let czy_O = true;
+
+function userClicked(cell) {
+    if(czy_O === true) {
+        if(document.getElementById(cell).innerHTML == "")
+        document.getElementById(cell).innerHTML = "O";
+        czy_O = false;
+    }else {
+        if(document.getElementById(cell).innerHTML == "")
+        document.getElementById(cell).innerHTML = "X";
+        czy_O = true;
+    }
 }
 
-function userClicked3() {
-    document.getElementById("3").innerHTML = "X"
-}
-
-function userClicked4() {
-    document.getElementById("4").innerHTML = "X"
-}
-
-function userClicked5() {
-    document.getElementById("5").innerHTML = "X"
-}
-
-function userClicked6() {
-    document.getElementById("6").innerHTML = "X"
-}
-
-function userClicked7() {
-    document.getElementById("7").innerHTML = "X"
-}
-
-function userClicked8() {
-    document.getElementById("8").innerHTML = "X"
-}
-
-function userClicked9() {
-    document.getElementById("9").innerHTML = "X"
-}
-                        ///END OTHER CLICKED (2-9)///
-var cell = 3;
-cell = cell*51241 + cell *3312/23 + 1
-console.log("Zmienna cell = ", cell)
